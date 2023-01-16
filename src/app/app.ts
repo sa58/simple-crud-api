191c752966e282a2bf7cca5df0266d149c5d387a
sa58 <likhacheva.392@gmail.com>
2023-01-17 00:27:34 +0300

    feat: add one request redirect

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,52 +13,16 @@ export const initializeApp = (request: IncomingMessage, response: ServerResponse
 
     console.log(process.pid)
 
-    const options = {
-        hostname: 'localhost',
-        port: 4001,
-        path: '/api/users',
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            // 'Content-Length': Buffer.byteLength(postData),
-        },
-    };
-
-    const req = http.request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        
-        res.on('data', (chunk) => {
-            console.log(`BODY: ${chunk}`);
-        });
-        
-        res.on('end', () => {
-            console.log('No more data in response.');
-        });
-    });
-      
-    req.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-    });
-      
-    // Write data to request body
-    // req.write(postData);
-    req.end();
-
-
-
-    // if (method && (url && isResourceValid(url, RESOURCE_USERS))) {
-    //     const userController = new UserController(url, method, response, request);
-    //     userController.setServiceMethod();
-    // } else {
-    //     throw new NotFoundError("Endpoint doesn't exist");
-    // }
+    if (method && (url && isResourceValid(url, RESOURCE_USERS))) {
+        const userController = new UserController(url, method, response, request);
+        userController.setServiceMethod();
+    } else {
+        throw new NotFoundError("Endpoint doesn't exist");
+    }
 }
 
 export const app = (request: IncomingMessage, response: ServerResponse) => {
     response.setHeader('Content-Type', 'application/json');
-
 
     handleError(() => initializeApp(request, response), response)();
 }
--- src/app/multi.ts
+++ src/app/multi.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 import cluster from 'node:cluster';
 import http, { IncomingMessage, ServerResponse } from 'node:http';
 import { cpus } from "node:os";
-import { bootstrap, initializeApp } from "./app";
+import { initializeApp } from "./app";
 
 dotenv.config();
 
@@ -20,69 +20,55 @@ if (cluster.isPrimary) {
     });
   
     http
-        .createServer(initializeApp)
+        .createServer((request: IncomingMessage, response: ServerResponse) => {
+            console.log(process.pid)
+
+            const options = {
+                hostname: 'localhost',
+                port: 4001,
+                path: '/api/users',
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // 'Content-Length': Buffer.byteLength(postData),
+                },
+            };
+        
+            const req = http.request(options, (res) => {
+                console.log(`STATUS: ${res.statusCode}`);
+                console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+                res.setEncoding('utf8');
+                // res
+                // res.on('data', (chunk) => {
+                //     console.log(`BODY: ${chunk}`);
+                // });
+                
+                // res.on('end', () => {
+                //     console.log('No more data in response.');
+                // });
+        
+                // response.end()
+
+                res.on('data', (chunk: any) => {
+                    response.end(chunk);
+                  });
+            });
+              
+            req.on('error', (e) => {
+                console.error(`problem with request: ${e.message}`);
+            });
+              
+            // Write data to request body
+            // req.write(postData);
+            req.end();
+        })
         .listen(4000, () => {
             console.log(`Master ${process.pid} is running`, process.env.port);
         })
-
-        // bootstrap1().listen(4000, '127.0.0.1', () => {
-        //     console.log(`\nServer pid: ${process.pid} started on port ${4000}`);
-        //     // @ts-ignore
-        //     for (const worker of Object.values(cluster.workers)) {
-        //         // @ts-ignore
-        //         console.log(worker.id)
-        //         // worker.send('big announcement to all workers');
-        //       }
-
-        // })
-    
 } else {
-    // Workers sharing same TCP connection
-
-    // bootstrap1().listen(process.env.NODE_PORT + cluster.worker.id, '127.0.0.1', () => {
-    //     console.log(`\nServer pid: ${process.pid} started on port ${process.env.NODE_PORT + cluster.worker.id}`);
-    // })
     http
         .createServer(initializeApp)
         .listen(process.env.port, () => {
-            console.log(`Master ${process.pid} is running`, process.env.port);
             console.log(`Worker ${process.pid} started`, process.env.port);
         })
-    
 }
-
-
-//   (request: IncomingMessage, response: ServerResponse) => {
-//     const options = {
-//         hostname: 'localhost',
-//         port: 4001,
-//         path: '/api/users',
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             // 'Content-Length': Buffer.byteLength(postData),
-//         },
-//     };
-
-//     const req = http.request(options, (res) => {
-//         console.log(`STATUS: ${res.statusCode}`);
-//         console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-//         res.setEncoding('utf8');
-        
-//         res.on('data', (chunk) => {
-//             console.log(`BODY: ${chunk}`);
-//         });
-        
-//         res.on('end', () => {
-//             console.log('No more data in response.');
-//         });
-//     });
-      
-//     req.on('error', (e) => {
-//         console.error(`problem with request: ${e.message}`);
-//     });
-      
-//     // Write data to request body
-//     req.write(postData);
-//     req.end();
-// }
